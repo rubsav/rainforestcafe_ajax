ba3405bc8abfd7418932048711397f832970991d
Ruben Avila <Ruavilaa@hotmail.com>
2014-11-24 15:03:38 -0500

    added a search method to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
+	before_filter :ensure_logged_in, only: [:show]
+ 
 	def index
-		@products = Product.all
+		@products = if params[:search]
+			Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
+		else
+			Product.all
+		end
 	end
 
 	def show
