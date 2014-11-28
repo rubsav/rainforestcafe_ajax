bd82102b2961fccdc56c9eedd857d05bd71f65c5
Ruben Avila <Ruavilaa@hotmail.com>
2014-11-27 22:04:20 -0500

    made sure infinite scroll works properly

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,9 +1,19 @@
-$(document).on('ready page:load', function() {
-	$('#search-form').submit(function(event) {
-		event.preventDefault();
-		var searchValue = $('#search').val();
-		
-		$.getScript('/products?search=' + searchValue);;
-	});
-});
+ $(document).ready(function() {
 
+    $('#search-form').submit(function(event) {
+      event.preventDefault();
+      var searchValue = $('#search').val();
+
+      $.getScript('/products?search=' + searchValue);
+    });
+
+    $(window).scroll(function() {
+      var url = $('.pagination span.next').children().attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+       $('.pagination').text("Fetching more products...");
+       console.log(url);
+       return $.getScript(url);
+     }
+   });
+
+  });
