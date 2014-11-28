47551f57609f857e7d4bdede0b08679a4a2614c5
Ruben Avila <Ruavilaa@hotmail.com>
2014-11-27 22:24:16 -0500

    modified ReviewsController#create method by adding the ability to respond to both standard requests and AJAX requests

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,20 +10,15 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.build(review_params)
     @review.user = current_user
 
-    # Check out this article on [.build](http://stackoverflow.com/questions/783584/ruby-on-rails-how-do-i-use-the-active-record-build-method-in-a-belongs-to-rel)
-    # You could use a longer alternate syntax if it makes more sense to you
-    # 
-    # @review = Review.new(
-    #   comment: params[:review][:comment],
-    #   product_id: @product.id,
-    #   user_id: current_user.id
-    # )
-
-    if @review.save
-      redirect_to products_path, notice: 'Review created successfully'
-    else
-      render 'products/show'
-    end
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to product_path(@product.id), notice: 'Review added.' }
+        format.js {} # This will look for app/views/reviews/create.js.erb
+      else
+        format.html { render 'products/show', alert: 'There was an error.'  }
+        format.js {} # This will look for app/views/reviews/create.js.erb
+      end
+    end    
   end
 
   def destroy
